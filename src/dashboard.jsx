d6c250a07f6916fd29ccecfeec853887c76aabb7
PutraSaputraa <161487538+PutraSaputraa@users.noreply.github.com>
2025-08-24 23:08:44 +0700

    update

--- src/dashboard.jsx
+++ src/dashboard.jsx
@@ -12,7 +12,7 @@ function Dashboard() {
                         alt="Foto Closeup Wajah" 
                         className="absolute fade-mask z-10"
                     />
-                    <h1 className="absolute bottom-40 left-2 font-poppins text-7xl font-bold text-blue-600 tracking-wider z-0">Anugraha</h1>
+                    <h1 className="absolute bottom-40 left-2 font-poppins text-5xl font-bold text-blue-600 tracking-wider z-0">Anugraha</h1>
                     <h1 className="absolute bottom-20 left-8 font-poppins text-7xl font-bold text-blue-500 tracking-wider z-0">Galih</h1>
                     <h1 className="absolute bottom-3 left-4 font-poppins text-7xl font-bold text-blue-600 tracking-wider z-20">Saputra</h1>
                 </div>
@@ -26,7 +26,7 @@ function Dashboard() {
                     <img 
                         src="/images/fotosma7.png" 
                         alt="SMA7" 
-                        className="h-[380px] absolute top-44 z-10 rounded-tl-[80px] rounded-br-[80px]"
+                        className="h-[300px] absolute top-44 z-10 rounded-tl-[80px] rounded-br-[80px]"
                     />
                     <h1 className="font-poppins text-3xl font-bold text-white mt-6 absolute bottom-12 left-5">SMAN 7 Yogyakarta</h1>
                 </div>
